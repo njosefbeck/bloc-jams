b96b7fc2954c038c5ff67f50ab25545532e34680
njosefbeck <njosefbeck@gmail.com>
2015-01-29 01:55:29 +0000

    Added action for off hover and cleaned up code for hover event

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,15 @@ $(document).ready(function() {
     $(this).text(subText + "!");
   });
 
-  $('.selling-points .point').hover(function() {
+  var onHoverAction = function(event) {
     console.log('Hover action triggered.');
     $(this).animate({'margin-top': '10px'});
-  });
+  };
+
+  var offHoverAction = function(event) {
+    console.log('Off-hover action triggered.');
+    $(this).animate({'margin-top': '0px'});
+  };
+
+  $('.selling-points .point').hover(onHoverAction, offHoverAction);
 });
