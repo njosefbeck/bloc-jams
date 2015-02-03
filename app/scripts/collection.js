d9804c2fafd8dc92bac2fab7466ad54da4c4cf80
njosefbeck <njosefbeck@gmail.com>
2015-02-03 01:13:24 +0000

    Added buildAlbumThumbnail function to collection.js

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -1,3 +1,22 @@
+var buildAlbumThumbnail = function () {
+  var template =
+      '<div class="collection-album-container col-md-2">'
+    + ' <img src="/images/album-placeholder.png"/>'
+    + ' <div class="caption album-collection-info">'
+    + '   <p>'
+    + '     <a class="album-name" href="/album.html"> Album Name </a>'
+    + '     <br/>'
+    + '     <a href="/album.html"> Artist name </a>'
+    + '     <br/>'
+    + '     X songs'
+    + '     <br/>'
+    + '   </p>'
+    + ' </div> <!-- end caption album-collection-info -->'
+    + '</div> <!-- end collection-album-container col-md-2 -->';
+
+    return $(template);
+};
+
 if (document.URL.match(/\/collection.html/)) {
   // Wait until the HTML is fully processed.
   $(document).ready(function() {
