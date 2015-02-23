261486409464db6485e2935f2c288d6dec8f7c67
njosefbeck <njosefbeck@gmail.com>
2015-02-23 01:56:09 +0000

    added selectTabHandler function to profile.js

---
+++ app/scripts/profile.js
@@ -0,0 +1,11 @@
+var tabsContainer = ".user-profile-tabs-container";
+var selectTabHandler = function(event) {
+  $tab = $(this);
+  $(tabsContainer + " li").removeClass('active');
+  $tab.parent().addClass('active');
+  selectedTabName = $tab.attr('href');
+  console.log(selectedTabName);
+  $(".tab-pane").addClass('hidden');
+  $(selectedTabName).removeClass('hidden');
+  event.preventDefault();
+};
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -367,7 +367,17 @@ $(document).ready(function() {
 });
 
 ;require.register("scripts/profile", function(exports, require, module) {
-
+var tabsContainer = ".user-profile-tabs-container";
+var selectTabHandler = function(event) {
+  $tab = $(this);
+  $(tabsContainer + " li").removeClass('active');
+  $tab.parent().addClass('active');
+  selectedTabName = $tab.attr('href');
+  console.log(selectedTabName);
+  $(".tab-pane").addClass('hidden');
+  $(selectedTabName).removeClass('hidden');
+  event.preventDefault();
+};
 });
 
 ;
