90be0060f985292cb61b25ce2742077d08fc7d38
njosefbeck <njosefbeck@gmail.com>
2015-02-25 01:42:04 +0000

    Added albumURLs array to landing page scope

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,5 +9,17 @@ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', funct
   $scope.subTextClicked = function() {
     $scope.subText += '!';
   };
+
+  $scope.albumURLs = [
+    '/images/album-placeholders/album-1.jpg',
+    '/images/album-placeholders/album-2.jpg',
+    '/images/album-placeholders/album-3.jpg',
+    '/images/album-placeholders/album-4.jpg',
+    '/images/album-placeholders/album-5.jpg',
+    '/images/album-placeholders/album-6.jpg',
+    '/images/album-placeholders/album-7.jpg',
+    '/images/album-placeholders/album-8.jpg',
+    '/images/album-placeholders/album-9.jpg',
+  ];
   
 }]);
