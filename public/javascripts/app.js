a0a6aaf73b3429bda127832999b913d6e11048ac
njosefbeck <njosefbeck@gmail.com>
2015-03-14 20:55:40 +0000

    Added album data to app.js to populate that data from controller

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -276,6 +276,39 @@ if (document.URL.match(/\/album.html/)) {
 //require("./album");
 //require("./profile");
 
+// Example album
+var albumPicasso = {
+  name: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: '/images/album-placeholder.png',
+
+  songs: [
+      { name: 'Blue', length: '4:26' },
+      { name: 'Green', length: '3:14' },
+      { name: 'Red', length: '5:01' },
+      { name: 'Pink', length: '3:21' },
+      { name: 'Magenta', length: '2:15' }
+  ]
+};
+
+// Another Example album
+var albumMarconi = {
+  name: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: '/images/album-placeholder2.png',
+
+  songs: [
+      { name: 'Hello, Operator?', length: '1:01' },
+      { name: 'Ring, ring, ring', length: '5:01' },
+      { name: 'Fits in your pocket', length: '3:21' },
+      { name: 'Wrong phone number', length: '2:15' }
+  ]
+};
+
 blocJams = angular.module('BlocJams', ['ui.router']);
 
 blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
