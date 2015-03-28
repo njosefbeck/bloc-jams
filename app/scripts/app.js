6b043955e41f1cb33e3e9eccf0dde3d5191e499c
njosefbeck <njosefbeck@gmail.com>
2015-03-28 18:06:28 +0000

    Created volumeClass function to dynamically update the icon as the volume changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -179,6 +179,14 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', 'ConsoleLogger'
 blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
 
+  $scope.volumeClass = function() {
+    return {
+      'fa-volume-off': SongPlayer.volume == 0,
+      'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+      'fa-volume-up': SongPlayer.volume > 70
+    }
+  }
+
   SongPlayer.onTimeUpdate(function(event, time){
     $scope.$apply(function(){
       $scope.playTime = time;
